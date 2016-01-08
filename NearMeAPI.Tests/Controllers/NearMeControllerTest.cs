46b26b6fa3bb00c984ffbc6522ff9a27b5cffd6d
Maksim Kanev <maks_kv@yahoo.com>
2016-01-09 01:27:58 +0200

    Added the Unit Tests to the project
    Note: Due to security reasons I have removed the Google API key. If you
    would like to get this service fully functional again, you would have to
    sign up with Google and get a new key.

---
+++ NearMeAPI.Tests/Controllers/NearMeControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NearMeAPI.Models;
+using NearMeAPI.Controllers;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.Web.SessionState;
+using System.Reflection;
+
+namespace NearMeAPI.Tests.Controllers
+{
+    [TestClass]
+    public class NearMeControllerTest
+    {
+        public readonly NearMeController controller = new NearMeController();
+
+        [TestMethod]
+        public void TestTypeSearch()
+        {
+            //HttpContext.Current = FakeHttpContext();
+            controller.SetIP("::1");
+
+            IHttpActionResult CorrectSearch = controller.TypeSearch("food");
+            OkNegotiatedContentResult<List<Place>> GooglePlaces = CorrectSearch as OkNegotiatedContentResult<List<Place>>;
+
+            Assert.IsNotNull(GooglePlaces);
+            Assert.IsNotNull(GooglePlaces.Content);
+
+            IHttpActionResult IncorrectSearch = controller.TypeSearch("munchies");
+            OkNegotiatedContentResult<List<Place>> NoPlaces = IncorrectSearch as OkNegotiatedContentResult<List<Place>>;
+
+            Assert.AreNotEqual(NoPlaces, GooglePlaces);
+        }
+
+        [TestMethod]
+        public void TestLocationSearch()
+        {
+            IHttpActionResult CorrectSearch = controller.LocationSearch("51.50013", "-0.126305", "food");
+            OkNegotiatedContentResult<List<Place>> GooglePlaces = CorrectSearch as OkNegotiatedContentResult<List<Place>>;
+
+            Assert.IsNotNull(GooglePlaces);
+            Assert.IsNotNull(GooglePlaces.Content);
+
+            IHttpActionResult IncorrectSearch = controller.LocationSearch("92.43001", "-194.208521", "food");
+            OkNegotiatedContentResult<List<Place>> NoPlaces = IncorrectSearch as OkNegotiatedContentResult<List<Place>>;
+
+            int expectedPlaces = 0;
+            Assert.AreEqual(expectedPlaces, NoPlaces.Content.Count);
+        }
+
+        // Creating a fake HttpContext
+        public static HttpContext FakeHttpContext()
+        {
+            var httpRequest = new HttpRequest("", "http://localhost/", "");
+            var stringWriter = new StringWriter();
+            var httpResponse = new HttpResponse(stringWriter);
+            var httpContext = new HttpContext(httpRequest, httpResponse);
+
+            var sessionContainer = new HttpSessionStateContainer("id", new SessionStateItemCollection(),
+                                                    new HttpStaticObjectsCollection(), 10, true,
+                                                    HttpCookieMode.AutoDetect,
+                                                    SessionStateMode.InProc, false);
+
+            httpContext.Items["AspSession"] = typeof(HttpSessionState).GetConstructor(
+                                        BindingFlags.NonPublic | BindingFlags.Instance,
+                                        null, CallingConventions.Standard,
+                                        new[] { typeof(HttpSessionStateContainer) },
+                                        null)
+                                .Invoke(new object[] { sessionContainer });
+
+            return httpContext;
+        }
+    }
+}
